03fc575e46c609f989790f1348584b4a29f4b722
[Zekhron] <[guillaume.bertomeu69@gmail.com]>
2020-10-22 09:34:22 +0200

    Add class, Developer, Game and Platform

---
+++ class/Developer.class.php
@@ -0,0 +1,69 @@
+<?php
+
+class Developer
+{
+    private $id;
+    private $name;
+    private $link;
+
+    public function __construct 
+    (
+        int $id,
+        string $name,
+        string $link
+    )
+
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->link = $link;
+    }
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of link
+     */ 
+    public function getLink()
+    {
+        return $this->link;
+    }
+
+    /**
+     * Set the value of link
+     *
+     * @return  self
+     */ 
+    public function setLink($link)
+    {
+        $this->link = $link;
+
+        return $this;
+    }
+}
